dc13a5727a2281e421f5b7dc3b57d6dffd4362ba
Justin Geeslin <justin@geesl.in>
2017-02-03 15:36:55 -0600

    Adding jquery to karma conf supporting the class’s use of extend

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'https://code.jquery.com/jquery-3.1.1.min.js',
 			'index.js',
   		'tests/DataStore.js'
     ],
