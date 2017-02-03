7022cd263ab57a5a098ce22d852b2f5c164f4f01
Justin Geeslin <justin@geesl.in>
2017-02-03 15:58:59 -0600

    Special Chrome flags for Travis

--- karma.conf.js
+++ karma.conf.js
@@ -61,6 +61,13 @@ module.exports = function(config) {
       'Chrome',
 		],
 
+    customLaunchers: {
+  		Chrome_travis_ci: {
+  				base: 'Chrome',
+  				flags: ['--no-sandbox ']
+  		},
+    }
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
@@ -69,4 +76,8 @@ module.exports = function(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   })
+
+	if(process.env.TRAVIS){
+		config.browsers = ['Chrome_travis_ci'];
+	}
 }
