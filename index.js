273fca521ff1434ffb1623712103604315a7802a
Justin Geeslin <justin@geesl.in>
2017-02-03 15:34:40 -0600

    Making the set function work with both strings and objects

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ DataStore = function (options) {
   var self = this;
   var dataStore = {};
 
-  this.set = function(data) {
-    dataStore = data;
+  this.set = function(key, value) {
+
+    if (Array.isArray(key)) {
+      console.error('Attempting to set data, but the first parameter should be either an object or a string. Found array.')
+      return false;
+    }
+    if (typeof key === "object") {
+      // Copy the values into the data Store
+      $.extend(dataStore, key)
+    }
+    else if (typeof key === 'string') {
+      dataStore[key] = value;
+
+      if (typeof value === 'undefined') {
+        console.error('Attempting to set data, but the value of the key-value pair is ' + typeof value)
+        return false;
+      }
+    }
+    else {
+      console.error('Attempting to set data, but the first parameter should be either an object or a string. Found ' + typeof key)
+      return false;
+    }
+    return true;
   }
 
   this.get = function(key) {
--- tests/DataStore.js
+++ tests/DataStore.js
@@ -9,33 +9,53 @@ describe('DataStore', function() {
 			expect(window.theData instanceof DataStore).toBe(true)
     });
 
-		it('should set values from an object passed as a parameter', function() {
-			theData.set({name: 'John'});
+		it('should set a value from a key value pair', function() {
+			theData.set('name', 'John');
 			// Get the value. Is it the same value?
 			expect(theData.get('name')).toBe('John');
     });
+
+		it('should set a value from an object passed as a parameter', function() {
+			theData.set({name: 'James'});
+			// Get the value. Is it the same value?
+			expect(theData.get('name')).toBe('James');
+    });
+
+		it('should return false if the first parameter of set is not either an object or a string key.', function() {
+			var returnValue = theData.set(['name'], 'John');
+			// Get the value. Is it the same value?
+			expect(returnValue).toBe(false);
+    });
+
+		it('should return false if the first parameter is a key but second parameter of set is neither an object or a string.', function() {
+			var returnValue = theData.set('name');
+			// Get the value. Is it the same value?
+			expect(returnValue).toBe(false);
+		});
 
 		it('should get a value using a key (string parameter)', function() {
 			// Set a value first
-			theData.set({name: 'John'});
+			theData.set('name', 'Peter');
 			// Get the value. Is it the same value?
-			expect(theData.get('name')).toBe('John');
+			expect(theData.get('name')).toBe('Peter');
     });
 
 		it('should get all data', function() {
+			theData.empty();
 			var allData = {'firstName': 'John', 'lastName': 'Doe'}
 			// Set a value first
 			theData.set(allData);
 			// Get the value. Is it the same value?
-			expect(theData.getAll()).toBe(allData);
+			expect(theData.getAll().toString()).toBe(allData.toString());
     });
 
 		it('should implement valueOf', function() {
+			theData.empty();
 			var allData = {'firstName': 'John', 'lastName': 'Doe'}
 			// Set a value first
 			theData.set(allData);
 			// Get the value. Is it the same value?
-			expect(theData.valueOf()).toBe(allData);
+			expect(theData.valueOf().toString()).toBe(allData.toString());
     });
 
 		it('should implement hasOwnProperty', function() {
