a4609604c48fcd611559c863f8ee59a090a9eecb
Justin Geeslin <justin@geesl.in>
2017-04-06 21:20:52 -0500

    Adding code climate coverage. Because what are tests without coverage?

--- karma.conf.js
+++ karma.conf.js
@@ -29,14 +29,24 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'index.js' : 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: [
+      'dots',
+      'coverage'
+    ],
 
+    coverageReporter: {
+      reporters: [
+        { type : 'lcov', dir : 'coverage/' },
+        { type : 'lcovonly', subdir : '.', file: 'lcov.info' }
+      ]
+    },
 
     // web server port
     port: 9876,
